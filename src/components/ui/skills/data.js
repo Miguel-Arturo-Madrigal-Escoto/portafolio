c50c69617a473898fbed495807efff3f55e2da9e
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2022-09-16 22:57:31 -0500

    habilidades agregadas

--- src/components/ui/skills/data.js
+++ src/components/ui/skills/data.js
@@ -9,7 +9,11 @@ import {
     SiReact,
     SiPython,
     SiBootstrap,
-    SiCplusplus
+    SiCplusplus,
+    SiExpress,
+    SiNodedotjs,
+    SiMongodb,
+    SiTypescript
 } 
 
     from 'react-icons/si';
@@ -21,15 +25,17 @@ const size = "3rem";
 
 const skills = [
     {
-        id: 'Desarrollo web',
+        id: 'Desarrollo',
         icon: <BsGlobe size={ size } style={{'color': '223e92'}} />,
         icons: [
             <SiHtml5 size={ size } style={{ 'color': 'e34c26' }} />,
             <SiCss3 size={ size } style={{ 'color': '264de4' }} />,
             <SiJavascript size={ size } style={{ 'color': 'F0DB4F' }} />,
             <SiPhp size={ size } style={{ 'color': '474A8A' }} />,
+            <SiNodedotjs size={ size } style={{ 'color': '44883e' }} />,
+            <SiTypescript size={ size } style={{ 'color': '007acc' }} />,
         ],
-        techs: [ 'Html', 'Css', 'Javascript', 'Php' ]
+        techs: [ 'Html', 'Css', 'Javascript', 'Php', 'Node.js', 'TypeScript' ]
     },
     {
         id: 'Frameworks y Librerias',
@@ -37,8 +43,9 @@ const skills = [
         icons: [
             <SiBootstrap size={ size } style={{ 'color': '602C50' }} />,
             <SiReact size={ size } style={{ 'color': '61DBFB' }}/>,
+            <SiExpress size={ size } style={{ 'color': '333333' }} />
         ],
-        techs: ['Bootstrap', 'React']
+        techs: ['Bootstrap', 'React', 'Express']
     },
     {
         id: 'Otros',
@@ -46,10 +53,11 @@ const skills = [
         icons: [
             <SiPython size={ size } style={{ 'color': '4B8BBE' }}/>,
             <SiMysql size={ size } style={{ 'color': 'F29111' }} />,
+            <SiMongodb size={ size } style={{ 'color': '3FA037' }} />,
             <SiCplusplus size={ size } style={{ 'color': '044F88' }}/>,
             <FaGitAlt size={ size } style={{ 'color': 'f34f29' }}/>,
         ],
-        techs: ['Python', 'MySQL', 'C/C++', 'Git']
+        techs: ['Python', 'MySQL', 'Mongo', 'C/C++', 'Git']
     }
 ];
 
